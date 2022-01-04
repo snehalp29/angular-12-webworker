15b122a079d1f41596925d55fb673fe3b1ee5b2e
snehalp29 <snehalpatkar@gmail.com>
2022-01-03 21:28:39 -0500

    App component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  numberValue = 100000;
+  result?: number;
+  startTime?: Date;
+  endTime?: Date;
+  uiBlockedFor?: number;
+  counter = 1;
+
+  WorkerCalPrime(num: number){
+    this.startTime = new Date();
+
+    if(typeof Worker !== 'undefined'){
+      // Create a new 
+      const worker = new Worker(new URL('./app.worker'));
+
+      worker.onmessage = ({data}) => {
+        console.log(`Page got message: ${data}`);
+        this.result = data.prime;
+      }
+      worker.postMessage({num});
+    }else{
+      console.log(`WebWorkers not supported`);
+    }
+
+    this.endTime = new Date();
+    this.uiBlockedFor = this.endTime.valueOf() - this.startTime.valueOf();
+
+  }
+
+  log(){
+    console.log(this.counter++);
+  }
+
 }
